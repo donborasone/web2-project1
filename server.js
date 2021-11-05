891992d6f2cca1f392c1f26bec931edb61c5bf39
Bartol Boras <bartol.boras@fer.hr>
2021-11-05 10:46:11 +0100

    Njihov primjer sklepan...

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+var fs = require('fs')
+var https = require('https')
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(express.static('public'));
+app.set('view engine', 'pug')
+
+const { auth, requiresAuth } = require('express-openid-connect'); 
+const port = 4080;
+
+const config = { 
+  authRequired : false,
+  idpLogout : true, //login not only from the app, but also from identity provider
+  secret: process.env.SECRET,
+  baseURL: `https://localhost:${port}`,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: 'https://dev-m1hgw9la.us.auth0.com',
+  clientSecret: process.env.CLIENT_SECRET,
+  authorizationParams: {
+    response_type: 'code' ,
+    //scope: "openid profile email"   
+   },
+};
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+app.get('/',  function (req, res) {
+    req.user = {
+        isAuthenticated : req.oidc.isAuthenticated()
+    };
+    if (req.user.isAuthenticated) {
+        req.user.name = req.oidc.user.name;
+    }
+    res.render('index', {user : req.user});
+});
+
+app.get('/private', requiresAuth(), function (req, res) {       
+    const user = JSON.stringify(req.oidc.user);      
+    res.render('private', {user}); 
+});
+
+app.get("/sign-up", (req, res) => {
+  res.oidc.login({
+    returnTo: '/',
+    authorizationParams: {      
+      screen_hint: "signup",
+    },
+  });
+});
+
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, app)
+  .listen(port, function () {
+    console.log(`Server running at https://localhost:${port}/`);
+  });
